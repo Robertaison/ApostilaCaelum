1a12ed7bedb41da3326f81a5f07ef6399d39cbfa
robertson.souza <robertson.souza@s2it.com.br>
2018-10-05 11:35:35 -0300

    Capitulo 11, refatorando código para fluxo completo

--- src/br/com/caelum/conta/Cliente.java
+++ src/br/com/caelum/conta/Cliente.java
@@ -6,15 +6,22 @@ public class Cliente {
 	private String Sobrenome;
 	private String cpf;
 	private int idade;
-	
-	public boolean validaCpf(String cpf) {
-		if(cpf==getCpf()) {
-			return true;
+
+	public Cliente(String nome, String sobrenome, String cpf, int idade) {
+		setNome(nome);
+		setSobrenome(sobrenome);
+		validarIdade(idade, cpf);
+	}
+
+	private void validarIdade(int idade, String cpf) {
+		if(idade>=18) {
+			setCpf(cpf);
+			setIdade(idade);
 		}else {
-			return false;
+			JOptionPane.showMessageDialog(null,  "Não é possível criar a conta, apenas maiores de 18 anos.");
 		}
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
@@ -36,23 +43,35 @@ public class Cliente {
 	}
 
 	public void setCpf(String cpf) {
-		if(getIdade()<=60) {
-			if(validaCpf(cpf)) {
-				setCpf(cpf);
-			}
-		}else if(getIdade()>60){
-			setCpf(cpf);
-		}else {
-			JOptionPane.showMessageDialog(null, "Cpf invalido, digite o cpf correto para operação");
-		}		
+		this.cpf = cpf;
 	}
-	
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	
+
 	public int getIdade() {
 		return idade;
 	}
-	
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public boolean validaCpf(String cpf) {
+		if(cpf==getCpf()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		String cliente = "Bem vindo ao Banco do Taison" +
+						 "----------------------------\n\n" +
+						 "Nome: " + getNome() + " " + getSobrenome() + "\n" +
+						 "Idade " + getIdade() + ", Cpf " + getCpf() +	"\n\nAcessar:\n" +
+						 "1. Conta Corrente" + "\n" +
+						 "2. Conta Poupança" + "\n\n" +
+						 "0. Sair";
+
+		return cliente;
+	}
 }
--- src/br/com/caelum/main/OOBasico.java
+++ src/br/com/caelum/main/OOBasico.java
@@ -6,12 +6,34 @@ import br.com.caelum.conta.Conta;
 import br.com.caelum.conta.ContaCorrente;
 import br.com.caelum.conta.ContaPoupanca;
 
+import java.util.ArrayList;
+
 
 public class OOBasico {
 
 	public static void main(String[] args) {
-		ContaPoupanca c1 = new ContaPoupanca("Ronin");
-		JOptionPane.showMessageDialog(null, c1);
+        int op=3;
+
+        while(op!=0){
+            op = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Digite uma opção." +
+                             "----------------\n\n" +
+                             "1. Cliente\n" +
+                             "2. Funcionário\n\n" +
+                             "0. Sair do  programa" +
+                             "-----------------\n\n"));
+
+            switch (op){
+                case 1:
+                    op = Integer.parseInt(JOptionPane.showInputDialog("1. Para acesssar a conta\n" +
+                                                                      "2. Para criar a nova conta\n\n" +
+                                                                      "0. sair"));
+                    if(op==1){
+
+                    }
+            }
+        }
+
 	}
 
 }
